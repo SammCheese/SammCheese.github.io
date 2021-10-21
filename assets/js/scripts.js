811a915b1fa29a34a20b058c41038a9ee9f24c23
Samm Cheese <sammcheese.github@gmail.com>
2021-10-21 03:40:00 +0200

    Social security number

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,11 +7,10 @@ function navigate(menuItems, page) {
   document.querySelector(menuItems).animate([
     { opacity: 1 },
     { opacity: 0 }
-  ],
-  1000);
+  ], 1000)
   setTimeout(() => {
     window.location.href = page;
-  }, 900);
+  }, 800);
   return;
 }
 
